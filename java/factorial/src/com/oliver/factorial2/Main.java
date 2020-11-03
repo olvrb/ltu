fd536d7c43acbc4b79ac671527f45ad5399d20d1
Oliver Boudet <oboudet1@me.com>
2020-11-03 14:11:35 +0100

    kassasystem

--- java/factorial/src/com/oliver/factorial2/Main.java
+++ java/factorial/src/com/oliver/factorial2/Main.java
@@ -3,7 +3,11 @@ package com.oliver.factorial2;
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println(factorial(10));
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(factorial(i));
+        }
     }
 
     private static int factorial (int num) {
---
+++ kassasystem/src/com/oliver/kassasystem/Main.java
@@ -0,0 +1,21 @@
+package com.oliver.kassasystem;
+
+import javax.sound.midi.SysexMessage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello, World!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
