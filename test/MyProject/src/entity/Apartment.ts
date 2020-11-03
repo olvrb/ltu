82aba302aceb0cb141908d0cb07ae5d77fa156db
Oliver Boudet <oboudet1@me.com>
2020-11-03 11:38:36 +0100

    restructure and delete : )

--- test/MyProject/src/entity/Apartment.ts
+++
@@ -1,5 +0,0 @@
-import { Entity, Column } from "typeorm";
-import { RentedProperty } from "./RentedProperty";
-
-@Entity()
-export class Apartment extends RentedProperty {}
--- test/MyProject/src/entity/Room.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column } from "typeorm";
-import { RentedProperty } from "./RentedProperty";
-
-@Entity()
-export class Room extends RentedProperty {
-    @Column()
-    public RoomNumber: string;
-}
