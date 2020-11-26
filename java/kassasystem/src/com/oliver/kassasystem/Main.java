e51950eee1befd40358a1409029ee7ada2a930ae
Oliver Boudet <oboudet1@me.com>
2020-11-26 08:39:59 +0100

    remove comments

--- java/kassasystem/src/com/oliver/kassasystem/Main.java
+++ java/kassasystem/src/com/oliver/kassasystem/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         CollectionOfMoney change = new CollectionOfMoney();
+
+        // Declare all types of possible bills & coins
         Money[] possibilities = new Money[] {
                 new Money(1000),
                 new Money(500),
@@ -35,84 +37,6 @@ public class Main {
             change.AddMoney(m);
         }
 
-        /*
-        int[] possibilities = new int[] {
-                1000,
-                500,
-                200,
-                100,
-                50,
-                20,
-                2,
-                1
-        };
-        for (int n : possibilities) {
-            while (calc >= n) {
-                switch (n) {
-                    case 1000 -> change.Tusen.Value++;
-                    case 500 -> change.FemHundra.Value++;
-                    case 200 -> change.TvaHundra.Value++;
-                    case 100 -> change.EttHundra.Value++;
-                    case 50 -> change.Femtio.Value++;
-                    case 20 -> change.Tjugio.Value++;
-                    case 2 -> change.Tva.Value++;
-                    case 1 -> change.Ett.Value++;
-                };
-                calc -= n;
-            }
-        }
-
-         */
-
-
-        /*
-
-        while (calc >= 1000) {
-            change.Tusen++;
-
-            calc -= 1000;
-        }
-
-        while (calc >= 500) {
-            change.FemHundra++;
-
-            calc -= 500;
-        }
-
-        while (calc >= 200) {
-            change.TvaHundra++;
-
-            calc -= 200;
-        }
-
-        while (calc >= 100) {
-            change.EttHundra++;
-            calc -= 100;
-        }
-        while (calc >= 50) {
-            change.Femtio++;
-
-            calc -= 50;
-        }
-        while (calc >= 20) {
-            change.Tjugio++;
-
-            calc -= 20;
-        }
-        while (calc >= 2) {
-            change.Tva++;
-
-            calc -= 2;
-        }
-        while (calc >= 1) {
-            change.Ett++;
-            calc--;
-        }
-
-         */
         System.out.println(change.Sum());
-
-        System.out.println(change);
-
     }
 }
