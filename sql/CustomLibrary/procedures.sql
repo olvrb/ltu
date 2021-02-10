e14f7f01eb3709a9efab379f165b44141fa54237
Oliver Boudet <oboudet1@me.com>
2021-02-10 15:08:49 +0100

    pop

--- sql/CustomLibrary/procedures.sql
+++ sql/CustomLibrary/procedures.sql
@@ -1,17 +1,5 @@
 USE CustomLibrary;
 
-DROP PROCEDURE IF EXISTS CreateCustomer;
-CREATE PROCEDURE CreateCustomer(IN Name VARCHAR(100),
-                                IN PersonNr VARCHAR(12),
-                                IN `Email` VARCHAR(1000),
-                                IN `Phone` VARCHAR(20),
-                                IN `Pincode` VARCHAR(4),
-                                IN `Type` ENUM ('RESEARCHER', 'TEACHER', 'STUDENT'))
-BEGIN
-    INSERT INTO Customer
-    VALUES (UUID(), Name, PersonNr, Email, Phone, Pincode, 0, Type);
-END;
-
 DROP TRIGGER IF EXISTS UpdateRentalUp;
 CREATE TRIGGER UpdateRentalUp
     AFTER INSERT
@@ -32,8 +20,8 @@ CREATE TRIGGER UpdateRentalDown
     ON Rental
     FOR EACH ROW
 BEGIN
-    IF old.Returned = new.Returned then
-        signal SQLSTATE '45000' set MESSAGE_TEXT = 'Book already returned.';
+    IF old.Returned = new.Returned THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Book already returned.';
     END IF;
     IF new.Returned = TRUE THEN
         UPDATE
@@ -50,7 +38,7 @@ DROP TRIGGER IF EXISTS MaxRental;
 CREATE TRIGGER MaxRental
     BEFORE INSERT
     ON Rental
-    FOR EACH ROW
+    FOR EACH ROW 
 BEGIN
     -- Get corresponding Customer
     SELECT C.Type, c.CurrentlyBorrowed, c.Pincode
@@ -62,15 +50,28 @@ BEGIN
 
     IF @customerType = 'RESEARCHER' THEN
         IF @currentlyBorrowed >= 20 THEN
-            SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Researchers can only borrow 20 books at once.';
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Researchers can only borrow 20 books at once.';
         END IF;
     ELSEIF @customerType = 'TEACHER' THEN
         IF @currentlyBorrowed >= 10 THEN
-            SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Teachers can only borrow 10 books at once.';
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Teachers can only borrow 10 books at once.';
         END IF;
     ELSEIF @customerType = 'STUDENT' THEN
         IF @currentlyBorrowed >= 5 THEN
-            SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Students can only borrow 5 books at once.';
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Students can only borrow 5 books at once.';
         END IF;
     END IF;
+END;
+
+
+DROP PROCEDURE IF EXISTS CreateCustomer;
+CREATE PROCEDURE CreateCustomer(IN Name VARCHAR(100),
+                                IN PersonNr VARCHAR(12),
+                                IN `Email` VARCHAR(1000),
+                                IN `Phone` VARCHAR(20),
+                                IN `Pincode` VARCHAR(4),
+                                IN `Type` ENUM ('RESEARCHER', 'TEACHER', 'STUDENT'))
+BEGIN
+    INSERT INTO Customer
+    VALUES (UUID(), Name, PersonNr, Email, Phone, Pincode, 0, Type);
 END;
